498cbbbb8a5ec1f69469b6d343bce6fcf8030902
Acena, Gary Dave <daveramirez200408@gmail.com>
2025-03-20 17:46:24 +0800

    Add files via upload

---
+++ assignment2.js
@@ -0,0 +1,11 @@
+// Iteration Mapping with Double Loops:
+
+let maxLimit = parseInt(prompt("Enter the Limit of the Double Loop: "));
+
+
+for (let i = 0; i <= maxLimit; i++) {
+    for (let j = 0; j <= maxLimit; j++) {
+        let addedValue = i + j;
+        console.log(`[${i}] [${j}] Added value is ${addedValue}`);
+    }
+}
